c284b426c2a812b25bb7dc26465c6d769a98ee5a
alperengonul <81569125+alperengonull@users.noreply.github.com>
2024-07-09 12:42:34 +0300

    up

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 import AuthContent from '../components/AuthContent/AuthContent'
+import Loading from '../components/Loading/Loading'
+
 
 export default function LoginScreen() {
+
+  const [isAuthanticating, setIsAuthanticating] = useState(false)
+  
+  async function loginHandler ({email, password}) {
+    setIsAuthanticating(true)
+    await login(email, password)
+    setIsAuthanticating(false)
+  }
+
+  if (isAuthanticating) {
+    return <Loading />
+  }
+
   return (
-    <AuthContent isLogin />
+    <AuthContent isLogin onAuthenticate={loginHandler} />
   )
 }
 
--- src/util/auth.js
+++ src/util/auth.js
@@ -2,14 +2,26 @@ import axios from 'axios'
 
 const API_KEY = 'AIzaSyDqxejf9KTj5KeY6oLvNsMfE4RQT0jQ82s'
 
-export async function createUser(email,password){
-    try {
-        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY, {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        });
-    } catch (error) {
-        console.log(error.response.data); // Hata mesajını ve detaylarını gösterir
-    }
+
+
+async function authenticate(mode,email,password)
+
+{
+    const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key= ${API_KEY}`, {
+        email: email,
+        password: password,
+        returnSecureToken: true
+    });
 }
+
+
+
+
+export async function createUser(email,password){
+   return authenticate('signup',email,password)
+}
+
+
+export async function login(email,password){
+    return authenticate('signInWithPassword',email,password)
+ }
